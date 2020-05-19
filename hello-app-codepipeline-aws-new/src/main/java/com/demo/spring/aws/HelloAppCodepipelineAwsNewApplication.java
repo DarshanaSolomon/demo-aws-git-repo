16efd3d571a4ca78d956c9bec3a388882c3aaa45
Darshana <Darshana@host.docker.internal>
2020-05-19 00:30:26 -0400

    commit

---
+++ hello-app-codepipeline-aws-new/src/main/java/com/demo/spring/aws/HelloAppCodepipelineAwsNewApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAppCodepipelineAwsNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAppCodepipelineAwsNewApplication.class, args);
+	}
+
+}
